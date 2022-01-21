817435d8d9f915e45301e2f1cac5fb3d1ceb2666
Mariot Tsitoara <85485914+mariot-wedge@users.noreply.github.com>
2022-01-21 08:54:29 +0000

    feat(843): remplacer la page de synthèse de l'offre

--- src/components/vehicle/form/FormVehicleFilters.vue
+++ src/components/vehicle/form/FormVehicleFilters.vue
@@ -1,43 +1,50 @@
 <template>
-  <div class="p-3 d-flex flex-column">
-    <div class="border-bottom mb-4 pb-3">
-      {{ $t("caareavlib.vehicle.search.refine_the_search") }}
-    </div>
-    <div v-for="criteria in Object.keys(value.filters)" :key="criteria">
-      <div class="my-1">{{ $t(`caareavlib.vehicle.search.${criteria}`) }}</div>
-      <FormSelect
-        v-if="value.filters[criteria].inputType === 'select'"
-        :name="`filter-${criteria}`"
+  <div class="p-3 form-vehicle-container">
+    <div class="form-vehicle-left">
+      <div
+        class="form-vehicle-input px-1"
+        v-for="criteria in Object.keys(value.filters)"
         :key="criteria"
-        :select-options="value.filters[criteria].choices"
-        :selected-option.sync="value.filters[criteria].value"
-        label-select-attr="label"
-        :allow-empty="true"
-        :empty-label="$t(`caareavlib.vehicle.search.all_${criteria}`)"
-        :multiple="true"
-        :disabled="Object.keys(value.filters[criteria].choices).length === 0"
-        @update:selected-option="
-          $emit('filter-input', criteria, value.filters[criteria])
-        "
-      ></FormSelect>
-      <FormInput
-        v-else
-        v-model="value.filters[criteria].value"
-        :label="$t('caareavlib.vehicle.search.model_year')"
-        :debounce-input="true"
-        :name="`filter-${criteria}`"
-        :label-class="['col-2']"
-        :type="value.filters[criteria].inputType"
-        :errors="filterErrors(criteria)"
-        @input="$emit('filter-input', criteria, value.filters[criteria])"
-      ></FormInput>
+      >
+        <div class="my-1 form-vehicle__label">
+          {{ $t(`caareavlib.vehicle.search.${criteria}`) }}
+        </div>
+        <FormSelect
+          v-if="value.filters[criteria].inputType === 'select'"
+          :name="`filter-${criteria}`"
+          :key="criteria"
+          :select-options="value.filters[criteria].choices"
+          :selected-option.sync="value.filters[criteria].value"
+          label-select-attr="label"
+          :allow-empty="true"
+          :empty-label="$t(`caareavlib.vehicle.search.all_${criteria}`)"
+          :multiple="true"
+          :disabled="Object.keys(value.filters[criteria].choices).length === 0"
+          @update:selected-option="
+            $emit('filter-input', criteria, value.filters[criteria])
+          "
+        ></FormSelect>
+        <FormInput
+          v-else
+          v-model="value.filters[criteria].value"
+          :label="$t('caareavlib.vehicle.search.model_year')"
+          :debounce-input="true"
+          :name="`filter-${criteria}`"
+          :label-class="['col-2']"
+          :type="value.filters[criteria].inputType"
+          :errors="filterErrors(criteria)"
+          @input="$emit('filter-input', criteria, value.filters[criteria])"
+        ></FormInput>
+      </div>
     </div>
-    <div
-      class="btn btn-outline-secondary mx-auto mt-3 font-size-12"
-      data-cy="vehicle-reset-filters"
-      @click.prevent="onEraseFilters"
-    >
-      {{ $t("caareavlib.vehicle.search.erase_filters") }}
+    <div class="d-flex align-self-end">
+      <button
+        class="btn btn-outline-secondary font-size-11 p-1 ml-4"
+        data-cy="vehicle-reset-filters"
+        @click.prevent="onEraseFilters"
+      >
+        {{ $t("caareavlib.vehicle.search.erase_filters") }}
+      </button>
     </div>
   </div>
 </template>
@@ -67,9 +74,14 @@ export default {
       return err
     },
     onEraseFilters() {
-      for (let filter in this.value.filters) {
-        // noinspection JSUnfilteredForInLoop
-        this.value.filters[filter].value = null
+      for (const filter in this.value.filters) {
+        if (Array.isArray(this.value.filters[filter].value)) {
+          // noinspection JSUnfilteredForInLoop
+          this.value.filters[filter].value = []
+        } else {
+          // noinspection JSUnfilteredForInLoop
+          this.value.filters[filter].value = ""
+        }
       }
       this.$emit("input", this.value)
       this.$emit("reset-filters")
